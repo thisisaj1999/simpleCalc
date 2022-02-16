1c86961df1ff382849d3cc3b384eaf137e9ec0ce
thisisaj1999 <atuljoshi94170@gmail.com>
2022-02-16 20:54:50 +0530

    exponential and modulus function added.

--- js/main.js
+++ js/main.js
@@ -13,8 +13,7 @@ document.querySelector('.sub').addEventListener('click', function () {
   const num1 = Number(document.querySelector('.val1').value);
   const num2 = Number(document.querySelector('.val2').value);
 
-  let ans;
-  num1 > num2 ? (ans = num1 - num2) : (ans = num2 - num1);
+  const ans = num1 > num2 ? num1 - num2 : num2 - num1;
 
   document.querySelector('.ans').textContent = ans;
 });
@@ -37,4 +36,21 @@ document.querySelector('.divide').addEventListener('click', function () {
   const ans = num1 > num2 ? num1 / num2 : num2 / num1;
   document.querySelector('.ans').textContent = ans;
   //you can add "Math.trunc(ans)" to remove digits after decimals
+});
+
+// Exponential
+document.querySelector('.exponential').addEventListener('click', function () {
+  const num1 = Number(document.querySelector('.val1').value);
+  const num2 = Number(document.querySelector('.val2').value);
+  const ans = num1 ** num2;
+  document.querySelector('.ans').textContent = ans;
+});
+
+// modulus
+document.querySelector('.modulus').addEventListener('click', function () {
+  const num1 = Number(document.querySelector('.val1').value);
+  const num2 = Number(document.querySelector('.val2').value);
+
+  const ans = num1 % num2;
+  document.querySelector('.ans').textContent = ans;
 });
